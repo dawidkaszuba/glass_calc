506f5845398cd4bfbf7ac24610d3c33d75a65ddb
dawidkaszuba <d.kaszuba89@gmail.com>
2019-04-11 23:44:34 +0200

    added frameController and Frame views

---
+++ src/main/java/pl/dawidkaszuba/glasscalc/controller/cms/FrameController.java
@@ -0,0 +1,61 @@
+package pl.dawidkaszuba.glasscalc.controller.cms;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import pl.dawidkaszuba.glasscalc.entity.Frame;
+import pl.dawidkaszuba.glasscalc.repository.FrameRepository;
+
+import javax.validation.Valid;
+
+@Controller
+@RequestMapping("/frame")
+public class FrameController {
+
+    @Autowired
+    private FrameRepository frameRepository;
+
+    @GetMapping("/add")
+    public String addFrame(Model model){
+        model.addAttribute("frame", new Frame());
+        return "cms/frame/add";
+    }
+    @PostMapping("/add")
+    public String saveFrame(@Valid Frame frame, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "cms/frame/add";
+        }else{
+            this.frameRepository.save(frame);
+            return "redirect:/frame/list";
+        }
+    }
+    @GetMapping("/list")
+    public String findAllFrame(Model model){
+        model.addAttribute("frames", this.frameRepository.findAll());
+        return "cms/frame/list";
+    }
+    @GetMapping("delete/{id}")
+    public String deleteFrame(@PathVariable Long id){
+        this.frameRepository.delete(this.frameRepository.findOne(id));
+        return "redirect:/frame/list";
+    }
+    @GetMapping("/edit/{id}")
+    public String editFrame(@PathVariable Long id, Model model){
+        model.addAttribute("frame", this.frameRepository.findOne(id));
+        return "cms/frame/edit";
+    }
+    @PostMapping("/saveEdited")
+    public String saveEditedFrame(@Valid Frame frame, BindingResult bindingResult){
+        if(bindingResult.hasErrors()){
+            return "redirect:/frame/edit/"+frame.getId();
+        }else{
+            this.frameRepository.save(frame);
+            return "redirect:/frame/list";
+        }
+    }
+}
--- src/main/java/pl/dawidkaszuba/glasscalc/entity/Frame.java
+++ src/main/java/pl/dawidkaszuba/glasscalc/entity/Frame.java
@@ -15,4 +15,49 @@ public class Frame {
     private double thickness;
     private double psi;
     private double price;
+
+    public Frame() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public double getThickness() {
+        return thickness;
+    }
+
+    public void setThickness(double thickness) {
+        this.thickness = thickness;
+    }
+
+    public double getPsi() {
+        return psi;
+    }
+
+    public void setPsi(double psi) {
+        this.psi = psi;
+    }
+
+    public double getPrice() {
+        return price;
+    }
+
+    public void setPrice(double price) {
+        this.price = price;
+    }
+
+
 }
